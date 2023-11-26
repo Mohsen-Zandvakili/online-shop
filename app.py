72ce48b2dca0416eb05f86cda996702408447a27
Mohsen-Zandvakili <vakilimohsen49@gmail.com>
2023-11-26 18:19:29 +0330

    origin

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 from blueprints.general import app as general
 from blueprints.admin import app as admin
 from blueprints.user import app as user
+import config
+import extentions
 
 
 app = Flask(__name__)
@@ -9,6 +12,15 @@ app = Flask(__name__)
 app.register_blueprint(general)
 app.register_blueprint(admin)
 app.register_blueprint(user)
+
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+# initialize the app with the extension
+extentions.db.init_app(app)
+
+with app.app_context():
+    extentions.db.create_all()
 
 
 if __name__ == '__main__':
